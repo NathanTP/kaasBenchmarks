c2b2db2ffc2af5d3edb0dbf2a2accb5dd8c1f64b
Nathan Pemberton <nathanp@berkeley.edu>
2021-07-22 14:37:17 -0700

    All code should now pass lint

--- inference/python/infbench/__init__.py
+++ inference/python/infbench/__init__.py
@@ -1,8 +1,7 @@
-
 # Someone imports tensorflow somewhere and it logs a bunch of junk, this
 # suppresses it
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-from . import dataset
-from . import model
+from . import dataset  # NOQA
+from . import model  # NOQA
