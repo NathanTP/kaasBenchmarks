2cd3b3e3dcfb6940aafaf9c928776e72b94b1a4c
Nathan Pemberton <nathanp@berkeley.edu>
2021-10-18 23:09:16 +0000

    Unify parsing for analysis code, rename n_completed to submission/completion_rate

--- inference/analysis/util.py
+++ inference/analysis/util.py
@@ -6,6 +6,33 @@ import pandas as pd
 import numpy as np
 
 
+def aggregateModels(fullResults, metric):
+    models = ['resnet50', 'bert', 'jacobi']
+    resDfs = {}
+    for model in models:
+        modelRes = [res for res in fullResults.values() if model in res['config']['model']]
+        if len(modelRes) == 0:
+            continue
+        maxNReplica = max([d['config']['n_replica'] for d in modelRes])
+
+        fullIndex = list(range(1, maxNReplica+1))
+        df = pd.DataFrame(index=fullIndex)
+
+        tvmRes = [res for res in modelRes if res['config']['model_type'] in ['tvm', 'direct']]
+        tvmSer = pd.Series([res[metric] for res in tvmRes], index=[res['config']['n_replica'] for res in tvmRes])
+        tvmSer = tvmSer.reindex(fullIndex)
+        df['Actor'] = tvmSer
+
+        kaasRes = [res for res in modelRes if res['config']['model_type'] == "kaas"]
+        kaasSer = pd.Series([res[metric] for res in kaasRes], index=[res['config']['n_replica'] for res in kaasRes], dtype=np.float64)
+        kaasSer = kaasSer.reindex(fullIndex)
+        df['KaaS'] = kaasSer
+
+        resDfs[model] = df
+
+    return resDfs
+
+
 def loadOneMlPerf(resPath):
     """Consolidate results from a single mlperf output directory into a pandas dataframe"""
     resDicts = []
@@ -13,21 +40,23 @@ def loadOneMlPerf(resPath):
         with open(resFile, 'r') as f:
             resDicts += json.load(f)
 
-    print(resPath)
-    print(len(resDicts))
     aggDict = {}
     # Configs are the same across all replicas
     aggDict['config'] = resDicts[0]['config']
-    aggDict['config']['nReplica'] = len(resDicts)
+    aggDict['config']['n_replica'] = len(resDicts)
 
-    aggDict['n_completed'] = sum([res['metrics']['n_completed'] for res in resDicts])
+    aggDict['completion_rate'] = sum([res['metrics']['completion_rate'] for res in resDicts])
+    # aggDict['submission_rate'] = sum([res['metrics']['submission_rate'] for res in resDicts])
 
     aggDict['latencies'] = np.array(list(itertools.chain.from_iterable([res['metrics']['latencies'] for res in resDicts])))
     aggDict['p50'] = np.quantile(aggDict['latencies'], 0.50) * 1000
     aggDict['p90'] = np.quantile(aggDict['latencies'], 0.90) * 1000
     aggDict['p99'] = np.quantile(aggDict['latencies'], 0.99) * 1000
+    aggDict['n_sample_total'] = len(aggDict['latencies'])
+    aggDict['n_mean_sample_pre_client'] = aggDict['n_sample_total'] / aggDict['config']['n_replica']
 
     return aggDict, resDicts
+
 
 def loadOneThroughput(resPath):
     resDicts = []
@@ -38,9 +67,10 @@ def loadOneThroughput(resPath):
     aggDict = {}
     aggDict['config'] = resDicts[0]['config']
     aggDict['throughput'] = sum(d['metrics']['throughput'] for d in resDicts)
-    aggDict['config']['nReplica'] = len(resDicts)
+    aggDict['config']['n_replica'] = len(resDicts)
 
     return aggDict, resDicts
+
 
 def loadAllThroughput(resPath):
     fullResults = {}
@@ -48,21 +78,7 @@ def loadAllThroughput(resPath):
         aggRes, _ = loadOneThroughput(resDir)
         fullResults[resDir.name] = aggRes
 
-    models = ['resnet50', 'bert', 'jacobi']
-    resDfs = {}
-    for model in models:
-        modelRes = [res for res in fullResults.values() if model in res['config']['model']]
-        modelRes = sorted(modelRes, key=lambda x: x['config']['nReplica'])
-        tvmThroughputs = [res['throughput'] for res in modelRes if res['config']['model_type'] in ["tvm", "direct"]]
-        kaasThroughputs = [res['throughput'] for res in modelRes if res['config']['model_type'] == "kaas"]
-
-        while len(tvmThroughputs) < len(kaasThroughputs):
-            tvmThroughputs.append(float('nan'))
-
-        resDfs[model] = pd.DataFrame({'Actor': tvmThroughputs, 'KaaS': kaasThroughputs},
-                                     index=range(1,len(tvmThroughputs) + 1))
-
-    return resDfs
+    return aggregateModels(fullResults, 'throughput')
 
 
 def loadAllMlPerf(resPath):
@@ -71,22 +87,7 @@ def loadAllMlPerf(resPath):
         aggRes, _ = loadOneMlPerf(resDir)
         fullResults[resDir.name] = aggRes
 
-    models = ['resnet50', 'bert']
-    resDfs = {}
-    for model in models:
-        modelRes = [res for res in fullResults.values() if model in res['config']['model']]
-        modelRes = sorted(modelRes, key=lambda x: x['config']['nReplica'])
-        tvmThroughputs = [res['n_completed'] for res in modelRes if res['config']['model_type'] in ["tvm", "direct"]]
-        kaasThroughputs = [res['n_completed'] for res in modelRes if res['config']['model_type'] == "kaas"]
-        # tvmThroughputs = [res['config']['scale'] for res in modelRes if res['config']['model_type'] == "tvm"]
-        # kaasThroughputs = [res['config']['scale'] for res in modelRes if res['config']['model_type'] == "kaas"]
-
-        while len(tvmThroughputs) < len(kaasThroughputs):
-            tvmThroughputs.append(float('nan'))
-
-        resDfs[model] = pd.DataFrame({'Actor': tvmThroughputs, 'KaaS': kaasThroughputs},
-                                     index=range(1, len(tvmThroughputs) + 1))
-    return resDfs
+    return aggregateModels(fullResults, 'completion_rate')
 
 
 def loadAllLatDist(resPath):
@@ -95,22 +96,7 @@ def loadAllLatDist(resPath):
         aggRes, _ = loadOneMlPerf(resDir)
         fullResults[resDir.name] = aggRes
 
-    models = ['resnet50', 'bert']
-    resDfs = {}
-    for model in models:
-        modelRes = [res for res in fullResults.values() if model in res['config']['model']]
-        modelRes = sorted(modelRes, key=lambda x: x['config']['nReplica'])
-        tvmRes = [res['p90'] for res in modelRes if res['config']['model_type'] == "tvm"]
-        kaasRes = [res['p90'] for res in modelRes if res['config']['model_type'] == "kaas"]
-
-        maxLen = max(len(tvmRes), len(kaasRes))
-        for res in [tvmRes, kaasRes]:
-            while len(res) < maxLen:
-                res.append(float('nan'))
-
-        resDfs[model] = pd.DataFrame({'Actor': tvmRes, 'KaaS': kaasRes},
-                                     index=range(1, len(tvmRes) + 1))
-    return resDfs
+    return aggregateModels(fullResults, 'p90')
 
 
 def minMaxThroughput(thrReport):
@@ -124,9 +110,9 @@ def minMaxThroughput(thrReport):
 if __name__ == "__main__":
     resPath = pathlib.Path(sys.argv[1])
 
-    model = 'jacobi'
+    model = 'bert'
     print(model)
-    # print(loadAllLatDist(resPath)[model])
+    print(loadAllLatDist(resPath)[model])
     # print(minMaxThroughput(loadAllThroughput(resPath)))
-    print(loadAllThroughput(resPath)[model])
+    # print(loadAllThroughput(resPath)[model])
     # print(loadAllMlPerf(resPath)['resnet50'])
