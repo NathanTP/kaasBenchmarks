e476faa244e87672e25f6bed92dd3b46ae781dcc
Nathan Pemberton <nathanp@berkeley.edu>
2022-07-13 12:46:06 -0700

    Cleanup comments/documentation after ASPLOS '22 paper push.

--- inference/benchmark/multiExperiment.py
+++ inference/benchmark/multiExperiment.py
@@ -12,9 +12,9 @@ from infbench import properties
 
 resultsDir = pathlib.Path("./results")
 
-# nReplicas = [2, 4, 6, 8]
-# models = ['cGEMM', 'jacobi', 'resnet50', 'bert']
-# expKeys = ['exclusive']
+nReplicas = [2, 4, 6, 8]
+models = ['cGEMM', 'jacobi', 'resnet50', 'bert']
+expKeys = ['exclusive']
 
 
 def keyToOpts(expKey):
@@ -182,10 +182,17 @@ def latDistribution(configs, suiteOutDir, fast=False, hetero=False):
 
 
 def latThr(configs, suiteOutDir, fast=False, hetero=False):
+    """For every configuration, sweep submission rates (fractions of the
+    measured peak throughput) and run the standard mlperf benchmark. This
+    records both achieved throughput and latency distributions at those
+    throughputs. The output directory structure is:
+        runN/model_mode_nClient/rateXX
+
+    Where XX is the submission rate as a percent of peak throughput. e.g.:
+    cGEMM_kaas_8/rate20 is cGEMM in kaas mode with 8 clients submitting at
+    20% of cGEMM's measured peak throughput (from properties.py)."""
     props = properties.getProperties()
-    # sweepScales = [0.2, 0.4, 0.6, 0.8]
-    #XXX
-    sweepScales = [0.2, 0.4]
+    sweepScales = [0.2, 0.4, 0.6, 0.8]
     for model, expKey, nReplica in configs:
         scales = getScales(props, model, expKey, nReplica, False, hetero)
         runTime = getTargetRuntime(nReplica, model, expKey, fast=fast)
@@ -207,6 +214,20 @@ def latThr(configs, suiteOutDir, fast=False, hetero=False):
             shutil.copytree(resultsDir / 'latest', runOutDir / f"rate{int(sweepScale*100)}",
                             ignore=shutil.ignore_patterns("*.ipc"))
 
+
+# This script is pretty manual. You go to the top of the file and set the
+# variables for model, expKeys, and nReplicas for the experiments you'd like to
+# run. The script will run the cartesian product of these using the experiment
+# you specify on the command line.
+#
+# All experiments will output into either the specified outDir or an
+# auto-generated output directory. You can re-use the same directory multiple
+# times, each run goes into the next available outDir/runX directory (where X
+# just increments from 0). You should only run one experiment type (i.e. 'lat' or
+# 'throughput') per output directory. The analysis scripts will automatically
+# aggregate results from all the runX directories. You can repeat the same
+# configuration to get more data points or run different configs at different
+# times.
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Full experiment suites")
     parser.add_argument('-e', '--experiment', choices=['throughput', 'lat', 'latThr', 'mlperf', 'nshot'])
